fd0e74d39f4dd32ddeb971aa0cfaecef837dab53
JoaogustavoKoche <joaogustavokoche@gmail.com>
2022-12-19 17:14:59 -0300

    Criação do repositorio Java

---
+++ primeiroProjeto/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int p = 2;
+
+        int o = 5;
+
+        int soma = (p + o);
+        System.out.println("Hello world! "+soma );
+    }
+}
+
